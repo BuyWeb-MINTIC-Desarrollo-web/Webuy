7b240e00015c3b1d4cfbe4de29df3ca99d0d5a5d
sebastiancs95 <jhonsebastian97@hotmail.es>
2022-11-20 14:54:00 -0500

    Conexión
    Se empeza realizando una conexión a un servidor local

--- Backend/server.js
+++ Backend/server.js
@@ -1,1 +1,9 @@
-const app =  require("app");
+const app=require("./app");
+
+//Setear mi archivo de configuración
+const dotenv = require("dotenv");
+dotenv.config({path: 'backend/config/config.env'})
+
+const server = app.listen(process.env.PORT, () => {
+    console.log(`Servidor iniciado en el puerto:  ${process.env.PORT} en modo: ${process.env.NODE_ENV}`)
+})
